302daeca06d8a3b9d57759bb8cb8f9f5fa2bd579
applenamu-mac <applenamu3@gmail.com>
2018-03-20 01:52:43 +0900

    QNA 작업 업데이트

--- manager/js/interactive/views/QnaView.js
+++ manager/js/interactive/views/QnaView.js
@@ -2,6 +2,7 @@ App.Views.QnaView = Backbone.View.extend({
 	initialize : function(obj){
 		this.addEvent();
 		this.container = $(".qna-con");
+		this.jsonData = ""
 	},
 
 	// 페이지 렌더링
@@ -20,6 +21,8 @@ App.Views.QnaView = Backbone.View.extend({
 		} else {
 			this.$el.addClass("alphaIn_animate");
 		}
+
+        this.getQnaData()
 
 	},
 
@@ -52,6 +55,10 @@ App.Views.QnaView = Backbone.View.extend({
 	// 이벤트 생성
 	addEvent : function(){
 		// this.$el.find(".btn-brochure").on(App.GlobalVars.CLICK, this.onClick_brochure)
+		this.$el.find(".btn-current").on(App.GlobalVars.CLICK, _.bind(this.onClick_current, this))
+		this.$el.find(".btn-all").on(App.GlobalVars.CLICK, _.bind(this.onClick_all, this))
+
+		this.$el.find(".btn-close-qnapop").on(App.GlobalVars.CLICK, _.bind(this.onClick_close, this))
 	},
 
 	// 이벤트 제거
@@ -66,22 +73,153 @@ App.Views.QnaView = Backbone.View.extend({
 		var h = App.GlobalVars.window_height;
 	},
 
+    onClick_current : function(e){
+		this.showContentsCurrent();
+		return false
+	},
+
+	onClick_all : function(e){
+		this.showContentsAll();
+		return false
+	},
+
+	showBtnCurrent : function(){
+        this.$el.find(".btn-current").removeClass("hide");
+	},
+
+	hideBtnCurrent : function(){
+        this.$el.find(".btn-current").addClass("hide");
+	},
+
+	showBtnAll : function(){
+        this.$el.find(".btn-all").removeClass("hide");
+	},
+
+	hideBtnAll : function(){
+        this.$el.find(".btn-all").addClass("hide");
+	},
+
+	showContentsCurrent : function(){
+		var current = App.GlobalVars.current_session_index;
+
+        this.hideBtnCurrent();
+        this.showBtnAll();
+
+        this.$el.find('.qna-session').addClass('hide');
+        this.$el.find('.qna-session').eq(current).removeClass('hide');
+
+	},
+
+	showContentsAll : function(){
+        this.hideBtnAll();
+        this.showBtnCurrent();
+
+        this.$el.find('.qna-session').removeClass('hide');
+	},
+
+	onClick_list : function(e){
+		var oneIndex = $(e.currentTarget).attr('data-one');
+		var twoIndex = $(e.currentTarget).attr('data-two');
+        this.setQnaPopup(oneIndex, twoIndex);
+		this.showPopup();
+		return false;
+	},
+
+	onClick_close : function(){
+		this.hidePopup()
+		return false;
+	},
+
+	showPopup : function(){
+
+		this.$el.find('.qna-pop').removeClass('hide');
+	},
+
+	hidePopup : function(){
+        this.$el.find('.qna-pop').addClass('hide');
+	},
+
+	setQnaPopup : function(one, two){
+        var session = this.getSession(one);
+		var text = session[two].value;
+		this.$el.find('.qna-pop p').text(text);
+	},
+
 	getQnaData : function(){
         var url = App.GlobalVars.GET_ADMIN_QNA_URL;
         if(App.GlobalVars.isDebugMode) url = App.GlobalVars.DEBUG_GET_ADMIN_QNA_URL;
 
         var lstno = App.GlobalVars.json_setting_data.lstno;
-        var sessno = App.GlobalVars.json_setting_data.qlist[App.GlobalVars.current_q_index].qno;
+        var sessno = App.GlobalVars.current_session_index+1;
         var data = {
             "lstno":lstno,
-            "sessno":current_session_index
+            "sessno":sessno+1
         }
 
-        App.getJsonData(url, data, this.onComplete_getQnaData)
+        App.getJsonData(url, data, _.bind(this.onComplete_getQnaData, this))
 	},
 
 	onComplete_getQnaData : function(json){
+		this.jsonData = json;
+		this.setQnaData(json);
+	},
 
+	setQnaData : function(json){
+
+        this.$el.find(".qna-area ol").children().remove();
+
+		var len = json.result.length;
+
+		for(var i = 0; i<len; i++){
+			var qLen = 0;
+			var session = this.getSession(i);
+
+
+            qLen = session.length;
+
+			/*<li class="qna-session">
+					<b>Session 2</b>
+				<ul>
+					<li><a href="#">ics laba lama 3가지 약제가 하나의 흡입기에 들어간 약제는 나오나요?</a></li>
+					<li><a href="#">경기도내 대표적인 신도시중 한 곳인 화성 동탄2신도시 23블록 부영아파트 입주민들이 입주 이후 5개월이 지나도록 하자보수가 답보상태에 머물고 있는 현실에 단단히 화가 났다.</a></li>
+					<li><a href="#">경기도내 대표적인 신도시중 한 곳인 화성 동탄2신도시 23블록 부영아파트 입주민들이 입주 이후 5개월이 지나도록 하자보수가 답보상태에 머물고 있는 현실에 단단히 화가 났다.</a></li>
+				</ul>
+			</li>		*/
+
+			var oneEle = '<li class="qna-session"><b>Session '+(i+1)+'</b><ul></ul></li>'
+
+			this.$el.find(".qna-area ol").append(oneEle)
+
+			console.log(qLen)
+			for(var j=0; j<qLen; j++){
+				var twoEle = '<li><a href="#" data-one="'+i+'" data-two="'+j+'">'+session[j].value+'</a></li>';
+                this.$el.find(".qna-area ol>li").eq(i).find(">ul").append(twoEle);
+			}
+
+		}
+
+        this.$el.find(".qna-area ol>li>ul>li>a").off(App.GlobalVars.CLICK);
+        this.$el.find(".qna-area ol>li>ul>li>a").on(App.GlobalVars.CLICK, _.bind(this.onClick_list,this));
+		this.showContentsCurrent();
+	},
+
+	getSession : function(index){
+        var result = this.jsonData.result[index];
+        var session = ""
+        if(index==0){
+            session = result.session1
+        }else if(index==1){
+            session = result.session2
+        }else if(index==2){
+            session = result.session3
+        }else if(index==3){
+            session = result.session4
+        }else if(index==4){
+            session = result.session5
+        }else if(index==5){
+            session = result.session6
+        }
+        return session
 	}
 
 
